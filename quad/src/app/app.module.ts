ad81811fdf94d9aae0e881d71a69db80f3a72ce9
amit singh <smamit27@gmail.com>
2018-07-01 18:29:22 +0530

    Route handling

--- quad/src/app/app.module.ts
+++ quad/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServersComponent } from './servers/servers.component';
 
-
+const appRoutes : Routes = [
+  { path: '', component : HomeComponent},
+  { path:'contact', component : ContactComponent},
+  { path:'servers', component : ServersComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
